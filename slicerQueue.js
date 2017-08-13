ef0ff2cd2ac8ed9d188be57e7d2c8e7f27bc1063
Mehdi Elhaij <mehdi.elhaij@hotmail.com>
2017-08-13 01:29:16 +0100

    kuejs slicing process

--- slicerQueue.js
+++ slicerQueue.js
@@ -1,1 +1,19 @@
-var slicer = require('./slicer');
+var slicer = require('./slicer'),
+    kue = require('kue'),
+    queue = require('kue').createQueue();
+
+
+////////////////////////////////////
+////// Settings
+////////////////////////////////////
+var concurrency = 10, // max active jobs 
+    timeToLive = 5*60*60*1000; // five hours
+
+// KueJs process
+queue.process('slice', concurrency, function (job, done) {  
+    slicer(job.data.id, function (fileId, fileInfo) {
+        console.log("done slicing " + job.data.title);
+        job.info = fileInfo;
+        done();
+    });
+});
