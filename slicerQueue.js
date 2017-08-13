ed60b586b529bd83a7535f29f14b11e74e8fdde5
Mehdi Elhaij <mehdi.elhaij@hotmail.com>
2017-08-13 01:30:04 +0100

    kuejs creating slicing jobs

--- slicerQueue.js
+++ slicerQueue.js
@@ -17,3 +17,23 @@ queue.process('slice', concurrency, function (job, done) {
         done();
     });
 });
+
+
+
+create_job = function(data, callback) {
+
+    var job = queue.create('slice', data).ttl(10 * 1000).ttl(timeToLive).save(
+        function (err) {
+            callback(job, err);
+        }
+    );
+    
+    job
+        .on('complete', function(){
+            console.log('Job ' + data.title + '(' + job.id + ') completed');
+        })
+        .on('failed', function() {
+            console.log('Job ' + data.title + '(' + job.id + ') failed');
+        });
+
+};
